793f2e19d6c6eafd853c8eac3fe83fe8b4f801cb
Jimmi Dyson <jimmidyson@gmail.com>
2017-05-08 09:46:02 +0100

    Bundle javascripts and CSS, with more optimizations

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,10 @@ var plugins = require('gulp-load-plugins')();
 
 var jsLibs = [
   './node_modules/scrollpos-styler/scrollPosStyler.js',
-  './themes/syndesis/js/connectingDots.js'
+  './themes/syndesis/js/connectingDots.js',
+  './node_modules/jquery/dist/jquery.slim.min.js',
+  './node_modules/tether/dist/js/tether.min.js',
+  './node_modules/bootstrap/dist/js/bootstrap.min.js'
 ];
 
 function sriTransformer(tHash) {
@@ -20,9 +23,7 @@ gulp.task('css', function () {
   return gulp.src('./themes/syndesis/scss/**/*.scss')
     .pipe(plugins.sass())
     .pipe(plugins.postcss())
-    .pipe(gulp.dest('./themes/syndesis/static/css'))
-    .pipe(plugins.sri({fileName: 'css.json', algorithms: ['sha384'], transform: sriTransformer}))
-    .pipe(gulp.dest('./data/sri/'));
+    .pipe(gulp.dest('./themes/syndesis/static/css'));
 });
 gulp.task('css:watch', gulp.series('css', function () {
   gulp.watch('./themes/syndesis/scss/**/*.scss', gulp.series('css'));
@@ -32,15 +33,20 @@ gulp.task('js', function () {
   return gulp.src(jsLibs)
     .pipe(plugins.concat('syndesis.js'))
     .pipe(plugins.uglifyjs())
-    .pipe(gulp.dest('./themes/syndesis/static/js'))
-    .pipe(plugins.sri({fileName: 'js.json', algorithms: ['sha384'], transform: sriTransformer}))
-    .pipe(gulp.dest('./data/sri/'));
+    .pipe(gulp.dest('./themes/syndesis/static/js'));
 });
 gulp.task('js:watch', gulp.series('js', function () {
-    gulp.watch(jsLibs, gulp.series('js'));
+  gulp.watch(jsLibs, gulp.series('js'));
+}));
+gulp.task('fonts', function() {
+  return gulp.src('./node_modules/font-awesome/fonts/*')
+    .pipe(gulp.dest('./themes/syndesis/static/fonts'))
+});
+gulp.task('fonts:watch', gulp.series('fonts', function () {
+  gulp.watch(['./node_modules/font-awesome/fonts/*'], gulp.series('fonts'));
 }));
 
-gulp.task('watch', gulp.parallel('css:watch', 'js:watch'));
+gulp.task('watch', gulp.parallel('fonts:watch', 'css:watch', 'js:watch'));
 
 gulp.task('hugo:serve', function (cb) {
   var exec = require('child_process').exec;
@@ -68,11 +74,13 @@ gulp.task('optimize-html', function() {
 });
 gulp.task('optimize-css', function() {
   return gulp.src(['./public/**/*.css'])
-    .pipe(plugins.purifycss(['./public/app/**/*.js', './public/**/*.html']))
+    .pipe(plugins.uncss({
+      html: ['./public/**/*.html']
+    }))
     .pipe(gulp.dest('./public'));
 });
 gulp.task('optimize', gulp.series('optimize-html', 'optimize-css'));
 
 gulp.task('serve', gulp.parallel('watch', 'hugo:serve'));
-gulp.task('build', gulp.series('css', 'js', 'hugo', 'optimize'));
+gulp.task('build', gulp.series('fonts', 'css', 'js', 'hugo', 'optimize'));
 gulp.task('default', gulp.series('build', 'serve'));
